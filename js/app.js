9937dbd3fcae51beb170ee84e65eacd080f8f746
Emerson Fang <efang@seas.harvard.edu>
2015-11-20 01:36:06 -0500

    enabled user to change color of canvas illustration based on input

--- js/app.js
+++ js/app.js
@@ -1,37 +1,84 @@
 // the application
+
 $(document).ready(function(){
-// Draw a black circle, line and an arc
-	$('#canvas_draw-btn').click(function(){
-		$('canvas').drawArc({
-		  layer: true,
-		  name: 'arc',
-		  fillStyle: 'black',
-		  x: 100, y: 120,
-		  radius: 20,
-		  start: 0, end: Math.PI*2,
-		  ccw: true,
-		  inDegrees: false
-		}).drawLine({
-		  layer: true,
-		  name: 'line',
-		  strokeStyle: '#000',
-		  strokeWidth: 10,
-		  x1: 115, y1: 30,
-		  x2: 115, y2: 120,
-		}).drawQuadratic({
-			layer: true,
-			name: 'quadratic',
-		  	strokeStyle: '#000',
-		  	strokeWidth: 5,
-		  	x1: 115, y1: 30, // Start point
-		  	cx1: 130, cy1: 50, // Control point
-		  	x2: 160, y2: 50 // End point
-		});
-	});//end click
+var colorval="black";
+// Draw a circle, line and an arc
+	$('#canvas_draw-btn').click(function draw(colorval){
+			$('canvas').drawArc({
+			  layer: true,
+			  name: 'arc',
+			  fillStyle: 'colorval',
+			  x: 100, y: 120,
+			  radius: 20,
+			  start: 0, end: Math.PI*2,
+			  ccw: true,
+			  inDegrees: false
+			}).drawLine({
+			  layer: true,
+			  name: 'line',
+			  strokeStyle: 'colorval',
+			  strokeWidth: 10,
+			  x1: 115, y1: 30,
+			  x2: 115, y2: 120,
+			}).drawQuadratic({
+				layer: true,
+				name: 'quadratic',
+			  	strokeStyle: 'colorval',
+			  	strokeWidth: 5,
+			  	x1: 115, y1: 30, // Start point
+			  	cx1: 130, cy1: 50, // Control point
+			  	x2: 160, y2: 50 // End point
+			})
+		});//end click
 
 	$('#canvas_clear-btn').click(function(){
 		$('canvas').clearCanvas();
 	});
+
+	//user's color choice
+	$('#presto').click(function(){
+		var colorval = $('#notecolor').val();
+		switch(colorval){
+			case 'Red':
+				colorval="red";
+				break;
+			case 'Pink':
+				colorval="pink";
+				break;
+			case 'Green':
+				color="green";
+				break;
+			default:
+				color="blue";
+		} //end switch
+		
+		//draw note with user's color choice
+		$('canvas').drawArc({
+			  layer: true,
+			  name: 'arc',
+			  fillStyle: colorval,
+			  x: 100, y: 120,
+			  radius: 20,
+			  start: 0, end: Math.PI*2,
+			  ccw: true,
+			  inDegrees: false
+			}).drawLine({
+			  layer: true,
+			  name: 'line',
+			  strokeStyle: colorval,
+			  strokeWidth: 10,
+			  x1: 115, y1: 30,
+			  x2: 115, y2: 120,
+			}).drawQuadratic({
+				layer: true,
+				name: 'quadratic',
+			  	strokeStyle: colorval,
+			  	strokeWidth: 5,
+			  	x1: 115, y1: 30, // Start point
+			  	cx1: 130, cy1: 50, // Control point
+			  	x2: 160, y2: 50 // End point
+			})
+	});//end click
 
 	$('#canvas_animate-btn').click(function(){
 		$('canvas').animateLayer('arc',{
